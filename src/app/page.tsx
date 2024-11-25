e1c29d6bed0e32077330c5b1a7c77e17c881db27
Claudio Tomas <ccaducarlos@gmail.com>
2024-11-25 23:55:47 +0200

    foto circular

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 "use client"
 // pages/index.tsx
+import Button from '@mui/material/Button';
 import { useRef, useState } from 'react';
 import Webcam from 'react-webcam';
+
 
 const Home = () => {
   const [userImage, setUserImage] = useState<string | null>(null);
@@ -42,35 +44,38 @@ const Home = () => {
     <div className='m-4'>
       <h1 className='text-red-500 text-center text-2xl'>Adicione os seus documentos</h1>
       <div className='flex gap-2'>
-        <button className='border px-1' onClick={() => handleCaptureClick('selfie')}>Take Selfie</button>
+        <Button className='normal-case' variant='outlined' onClick={() => handleCaptureClick('selfie')}>Tire a selfie Selfie</Button>
         <button className='border px-1' onClick={() => handleCaptureClick('id-front')}>Capture ID Front</button>
         <button className='border px-1' onClick={() => handleCaptureClick('id-back')}>Capture ID Back</button>
       </div>
+      <h3>Tire uma foto da sua cara</h3>
       <div className='flex justify-center mt-10'>
-      {currentCapture && (
-            <div>
-              <div className={`webcam-container h-52 w-52  ${facingMode === "user" ? "rounded-full flex items-center justify-center" : ""}`}>
-                <Webcam
-                className='h-60 w-52 rounded-2xl webcam-container'
-                  audio={false}
-                  ref={webcamRef}
-                  screenshotFormat="image/png"
-                  mirrored={facingMode === 'user'}
-                  
-                  videoConstraints={{ facingMode }}
-                />
-              </div>
-              <button onClick={capture}>Capture</button>
+        {currentCapture && (
+          <div className='flex justify-center flex-col gap-4'>
+            <div className={`h-80 w-80 rounded-full overflow-hidden border-4 border-gray-300  ${facingMode === "user" ? "rounded-full flex items-center justify-center" : ""}`}>
+              <Webcam
+                className='h-full w-full object-cover'
+                audio={false}
+                ref={webcamRef}
+                screenshotFormat="image/png"
+                mirrored={facingMode === 'user'}
 
+                videoConstraints={{ facingMode }}
+              />
             </div>
-
-          )}
+            <Button variant='outlined' onClick={capture}>Estou pronto (Cheeseeee!)</Button>
+          </div>
+        )}
       </div>
-      <div>
-        <h3>Captured Images:</h3>
-        {userImage && <img src={userImage} alt="User selfie" />}
-        {idFrontImage && <img src={idFrontImage} alt="ID Front" />}
-        {idBackImage && <img src={idBackImage} alt="ID Back" />}
+      <div className='text-center flex flex-col justify-center'>
+        <h3>Deseja tirar outra foto?</h3>
+        <div className='h-80 w-80 rounded-full overflow-hidden border-4 border-gray-300'>
+          {userImage && <img src={userImage} alt="User selfie" />}
+
+        </div>
+
+        {/* {idFrontImage && <img src={idFrontImage} alt="ID Front" />}
+        {idBackImage && <img src={idBackImage} alt="ID Back" />} */}
       </div><div>
         <h3>Captured Images:</h3>
         {userImage && <img src={userImage} alt="User selfie" />}
